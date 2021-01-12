b44c255b746ae9ccec35a8236ecbe03055aaa9b4
zhanghuan <2387749855@qq.com>
2021-01-12 08:53:19 +0800

    react中lazy和suspense使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,35 @@
-import React, {Component, createContext} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react'
 
-const BatteryContext = createContext(90);
-const OnlineContext = createContext(false);
+const AppTitle = lazy(
+    () => import(/* webpackChunkName: "title" */'./title.jsx')
+)
 
-class Leaf extends Component {
-    static contextType = BatteryContext;
-
-    render() {
-        const battery = this.context;
-        return (
-            <h1>Battery: {battery}</h1>
-        )
-    }
-}
-
-class Middle extends Component {
-    render() {
-        return <Leaf/>
-    }
-}
-
-class App extends Component {
+class App extends React.Component {
     state = {
-        battery: 60,
-        online: false
-    };
+        isError: false
+    }
 
-    render() {
-        const {battery, online} = this.state;
+    static getDerivedStateFromError(error) {
+        return { isError: true };
+    }
+
+    componentDidCatch (err, info) {
+        console.log("err>>>", err, info)
+    }
+
+    render () {
+        if (this.state.isError) {
+            return (<div>error</div>)
+        }
         return (
-            <BatteryContext.Provider value={battery}>
-                <OnlineContext.Provider value={online}>
-                    <button
-                        type="button"
-                        onClick={() => this.setState({battery: battery - 1})}>
-                        Press
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => this.setState({online: !online})}>
-                        Switch
-                    </button>
-                    <Middle/>
-                </OnlineContext.Provider>
-            </BatteryContext.Provider>
+            <div>
+                <Suspense fallback={<div>Loading</div>}>
+                    <AppTitle/>
+                </Suspense>
+            </div>
         )
     }
 }
 
-export default App;
+
+export default App
