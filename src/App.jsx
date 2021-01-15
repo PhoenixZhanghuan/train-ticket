25877430d507e8111feb50eee9c59d472a23aabc
zhanghuan <2387749855@qq.com>
2021-01-15 18:37:33 +0800

    Effect Hooks的使用方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,116 @@
-import React, {Component, memo} from 'react'
+import React, {Component, useEffect, useState} from 'react'
 
-const Foo = memo(function Foo(props) {
 
-    console.log('Foo render');
-    return null;
-
-});
-
-class App extends Component {
+class App2 extends Component {
 
     state = {
         count: 0,
+        size: {
+            width: 401,
+            height: 718
+        }
+    }
+
+    onResize = () => {
+        this.setState({
+            size: {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight
+            }
+        })
+    }
+
+    componentDidMount() {
+        document.title = this.state.count;
+
+        window.addEventListener('resize', this.onResize, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize, false)
+    }
+
+    componentDidUpdate() {
+        document.title = this.state.count;
     }
 
     render() {
-
+        const {count, size} = this.state;
         return (
             <div>
                 <button onClick={() => this.setState({count: this.state.count + 1})}>
                     Add
                 </button>
-                <Foo name="Mike" />
+                Click ({count})
+                size: {size.width}x{size.height}
             </div>
         )
     }
+}
+
+
+function App() {
+
+    const [count, setCount] = useState(0)
+
+    const [size, setSize] = useState({
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+    })
+
+    const onResize = () => {
+        setSize({
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        })
+    }
+
+    useEffect(() => {
+        console.log("111111")
+        document.title = count;
+    })
+
+    useEffect(() => {
+        console.log('count', count);
+    }, [count])
+
+    useEffect(() => {
+        console.log("222222")
+        window.addEventListener('resize', onResize, false)
+
+        return () => {
+            window.removeEventListener('resize', onResize, false)
+        }
+    }, [])
+
+    const onClick = () => {
+        console.log('click');
+    }
+
+    useEffect(() => {
+        document.querySelector('#size').addEventListener('click', onClick, false);
+        return () => {
+            document.querySelector('#size').removeEventListener('click', onClick, false);
+        }
+    })
+
+    return (
+        <div>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>
+                Count ({count})
+            </button>
+
+            {
+                count % 2
+                    ? <span id="size">size: {size.width}x{size.height}</span>
+                    : <p id="size">size: {size.width}x{size.height}</p>
+            }
+
+        </div>
+    )
+
 }
 
 
