1af9f2cbb1abd79de1ef986317c82c5304e5afe5
zhanghuan <2387749855@qq.com>
2021-01-19 15:06:18 +0800

    项目初始化

---
+++ src/index/App.jsx
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import './App.css';
+
+function App(props) {
+    return (
+        <div>
+            App
+        </div>
+    )
+}
+
+export default connect(
+    function mapStateToProps(state) {
+
+    },
+    function mapDispatchToProps(dispatch) {
+
+    }
+)(App);
