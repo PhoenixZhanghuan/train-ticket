15411b662958e13b1b5ba49f88754070789dca7a
zhanghuan <2387749855@qq.com>
2021-01-24 08:22:34 +0800

    添加搜索结果页数据结构

---
+++ src/common/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Nav.css';
+
+export default function Nav(props) {
+    return null;
+}
+Nav.propTypes = {
+
+}
--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,19 +1,30 @@
+import React from 'react';
 import {connect} from 'react-redux';
+
+import Nav from '../common/Nav.jsx'
+import List from './List.jsx';
+import Bottom from "./Bottom";
 import './App.css';
 
 function App(props) {
+    // const {
+    //
+    // } = props;
+
     return (
         <div>
-            App
+            <Nav />
+            <List />
+            <Bottom />
         </div>
     )
 }
 
 export default connect(
     function mapStateToProps(state) {
-
+        return state;
     },
     function mapDispatchToProps(dispatch) {
-
+        return {dispatch};
     }
 )(App);
